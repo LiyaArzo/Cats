75f4bcbd60fda4a25e7ff52bb47f2e721dafd340
LiyaArzo <chiffa.rozali77@gmail.com>
2024-11-04 16:28:37 +0500

    Создано окно и виджеты

--- cataas.py
+++ cataas.py
@@ -1,1 +1,23 @@
 from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO #importoutput
+
+def load.image(url):
+
+
+window = Tk()
+window.title('Cats')
+window.geometry('600x480')
+
+label = Label()
+label.pack()
+
+url = 'https://cataas.com/cat'
+img = load.image(url)
+
+if img:
+    label.config(image=img)
+    label.image = img #чтобы сборщик мусора не убрал картинку
+
+window.mainloop()
