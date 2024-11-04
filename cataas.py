0667d416788b24728980e6497a04ad409b261815
LiyaArzo <chiffa.rozali77@gmail.com>
2024-11-04 17:17:32 +0500

    Добавлено меню вместо кнопки

--- cataas.py
+++ cataas.py
@@ -6,10 +6,10 @@ from io import BytesIO #importoutput
 def loade_image(url):
     try:
         response = requests.get(url)
-        response.raise_for_status() #если будет ошибка, тут ее получим, для обработки исключения
-        image_data = BytesIO(response.content) #байт символы
+        response.raise_for_status() # если будет ошибка, тут ее получим, для обработки исключения
+        image_data = BytesIO(response.content) # байт символы
         img = Image.open(image_data)
-        img.thumbnail((600,480), Image.Resampling.LANCZOS) #корректировка размеров изображения, чтобы оно умещалось в окно
+        img.thumbnail((600,480), Image.Resampling.LANCZOS) # корректировка размеров изображения, чтобы оно умещалось в окно
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f'Произошла ошибка {e}')
@@ -23,15 +23,29 @@ def set_image():
         label.config(image=img)
         label.image = img  # чтобы сборщик мусора не убрал картинку
 
+
+def exit():
+    window.destroy()
+
+
 window = Tk()
 window.title('Cats')
 window.geometry('600x520')
 
+mainmenu = Menu(window)
+window.config(menu=mainmenu)
+
+filemenu = Menu(mainmenu, tearoff=0) # tearoff меню предопределено, его нельзя менять
+mainmenu.add_cascade(label="Файл", menu=filemenu) # название меню
+filemenu.add_command(label="Новый котик", command=set_image)
+filemenu.add_separator() # разделяющая линия
+filemenu.add_command(label="Выход", command=exit)
+
+
+
+
 label = Label()
 label.pack()
-
-update_button = Button(text='Обновить', command=set_image)
-update_button.pack()
 
 url = 'https://cataas.com/cat'
 set_image()
