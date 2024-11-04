cdf16d60dc36719831cafb41aaa16b555e18c48a
LiyaArzo <chiffa.rozali77@gmail.com>
2024-11-04 16:35:40 +0500

    Создана функция получения картинки по запросу

--- cataas.py
+++ cataas.py
@@ -3,7 +3,16 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO #importoutput
 
-def load.image(url):
+def loade_image(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status() #если будет ошибка, тут ее получим, для обработки исключения
+        image_data = BytesIO(response.content) #байт символы
+        img = Image.open(image_data)
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+        print(f'Произошла ошибка {e}')
+        return None
 
 
 window = Tk()
@@ -14,7 +23,7 @@ label = Label()
 label.pack()
 
 url = 'https://cataas.com/cat'
-img = load.image(url)
+img = loade_image(url)
 
 if img:
     label.config(image=img)
